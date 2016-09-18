b2c7bda46839299f0f3aa9c915c2526fa2161276
Andrés Arana <and2arana@gmail.com>
2016-09-18 12:29:20 +0200

    Sets up the webpack build system

---
+++ config/webpack.dev.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './app/index.jsx',
+  ],
+
+  resolve: {
+    root: [
+      path.resolve('./app'),
+    ],
+    extensions: [
+      '',
+      '.js',
+      '.jsx'
+    ],
+  },
+
+  output: {
+    path: './build',
+    filename: 'bundle.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel!eslint',
+      },
+
+      {
+        test: /\.sass$/,
+        loader: 'style!css?sourceMap!sass?sourceMap',
+      },
+
+      {
+        test: /\.(ttf|eot|svg|woff|woff2)/,
+        loader: 'file',
+      }
+    ]
+  },
+
+  devtool: 'cheap-module-eval-source-map',
+
+  plugins: [
+    new HtmlPlugin({
+      template: './app/index.html',
+    }),
+
+    new CopyPlugin([
+      {from: './app/favicons', to: './'},
+    ]),
+  ],
+};
+
