b8ea47e25449a2074dabbf4e283531cb171a8f3d
Sergio Matias Piano <smpiano@gmail.com>
2018-04-05 01:27:03 -0300

    Popup showing statement in mst

--- app/ui/stack.jsx
+++ app/ui/stack.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Menu, Header } from "semantic-ui-react";
+import { Popup, Container, Menu, Header } from "semantic-ui-react";
+import JSONTree from "react-json-tree";
 
 export const Stack = props => {
   return (
@@ -7,15 +8,28 @@ export const Stack = props => {
       <Header as="h5" textAlign="center">
         {props.name}
       </Header>
-      <Menu color={props.color} fluid vertical key={props.name}>
-        {props.container
-          .reduce((acc, item) => acc.concat(item), [])
-          .map((ss, indx) => (
-            <Menu.Item key={indx} active={props.indx == 0}>
-              {ss.statement.type}
-            </Menu.Item>
-          ))}
-      </Menu>
+      {props.container
+        .reduce((acc, item) => acc.concat(item), [])
+        .map((ss, indx) => (
+          <Menu color={props.color} fluid vertical key={props.name}>
+            <Popup
+              key={indx}
+              trigger={
+                <Menu.Item active={props.indx == 0}>
+                  {ss.statement.type}
+                </Menu.Item>
+              }
+              flowing
+              hoverable
+            >
+              <JSONTree
+                data={props.container
+                  .reduce((acc, item) => acc.concat(item), [])
+                  .map(ss => ss.statement)}
+              />
+            </Popup>
+          </Menu>
+        ))}
     </Container>
   );
 };
