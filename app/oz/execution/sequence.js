f1fa93e39d375cce79bcad7aadc8a9521034c090
Andres <and2arana@gmail.com>
2018-04-22 15:24:37 -0300

    Implements multisequence statement execution

--- app/oz/execution/sequence.js
+++ app/oz/execution/sequence.js
@@ -1,15 +1,25 @@
+import Immutable from "immutable";
+import { statementTypes } from "../machine/statements";
 import { buildSemanticStatement } from "../machine/build";
+
+const collectStatements = statement => {
+  if (statement.get("type") !== statementTypes.sequence) {
+    return Immutable.List.of(statement);
+  }
+  const head = Immutable.List.of(statement.get("head"));
+  const tail = collectStatements(statement.get("tail"));
+  return head.concat(tail);
+};
 
 export default function(state, semanticStatement, activeThreadIndex) {
   const statement = semanticStatement.get("statement");
   const environment = semanticStatement.get("environment");
-  const head = statement.get("head");
-  const tail = statement.get("tail");
+
+  const semanticStatements = collectStatements(statement).map(s =>
+    buildSemanticStatement(s, environment),
+  );
 
   return state.updateIn(["threads", activeThreadIndex, "stack"], stack =>
-    stack.push(
-      buildSemanticStatement(head, environment),
-      buildSemanticStatement(tail, environment),
-    ),
+    stack.concat(semanticStatements),
   );
 }
