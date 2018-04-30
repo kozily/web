549aba99688b1fdfe2626f5c3fc22389167b034b
Sergio Matias Piano <smpiano@gmail.com>
2018-04-30 15:06:33 -0300

    Implements the handling of spaces and comments

--- specs/parser/whitespaces_spec.js
+++ specs/parser/whitespaces_spec.js
@@ -1,6 +1,12 @@
 import Immutable from "immutable";
-import { skipStatementSyntax } from "../../app/oz/machine/statementSyntax";
+import {
+  skipStatementSyntax,
+  localStatementSyntax,
+  procedureApplicationStatementSyntax,
+} from "../../app/oz/machine/statementSyntax";
+import { lexicalIdentifier } from "../../app/oz/machine/lexical";
 import parse from "../../app/oz/parser";
+import { identifierExpression } from "../../app/oz/machine/expressions";
 
 describe("Parsing whitespace around statements", () => {
   beforeEach(() => {
@@ -9,5 +15,44 @@ describe("Parsing whitespace around statements", () => {
 
   it("handles it correctly", () => {
     expect(parse("   skip \n\t  ")).toEqual(skipStatementSyntax());
+  });
+
+  it("handles comments correctly", () => {
+    expect(parse("   skip % this is a comment \n\t  ")).toEqual(
+      skipStatementSyntax(),
+    );
+  });
+
+  it("handles mixed inline comments correctly", () => {
+    expect(
+      parse(
+        "  local X \n\r% this is a nested comment \n\r% local X \n\rin skip %this is a comment\n\r end ",
+      ),
+    ).toEqual(
+      localStatementSyntax([lexicalIdentifier("X")], skipStatementSyntax()),
+    );
+  });
+
+  it("handles nested comments correctly", () => {
+    expect(
+      parse(
+        " /*  local X \n\r this is a nested comment \n\r */ local X in skip end ",
+      ),
+    ).toEqual(
+      localStatementSyntax([lexicalIdentifier("X")], skipStatementSyntax()),
+    );
+  });
+
+  it("handles question mark in parameter as documentation correctly", () => {
+    expect(parse("{Sum A B ?C}")).toEqual(
+      procedureApplicationStatementSyntax(
+        identifierExpression(lexicalIdentifier("Sum")),
+        [
+          identifierExpression(lexicalIdentifier("A")),
+          identifierExpression(lexicalIdentifier("B")),
+          identifierExpression(lexicalIdentifier("C")),
+        ],
+      ),
+    );
   });
 });
